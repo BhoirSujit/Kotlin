fe78cfc7dc28c80a1e59659da9ebdb3e5e6e6f59
BhoirSujit <sujitbhoir004@gmail.com>
2023-01-13 08:25:04 +0530

    13-01-2023

---
+++ Immutable Variables.kt
@@ -0,0 +1,17 @@
+fun main()
+{
+    val name = "sujit"
+    val age = 19
+    println(name)
+    println(age)
+
+    //try to modify values
+///    name = "Shreyash"
+///    age = 17
+    println("Name is $name and age is $age")
+}
+
+/*
+Output
+Kotlin: Val cannot be reassigned
+ */
---
+++ Mutable Variables.kt
@@ -0,0 +1,19 @@
+fun main()
+{
+    var name = "sujit"
+    var age = 19
+    println(name)
+    println(age)
+
+    //modify values
+    name = "Shreyash"
+    age = 17
+    println("Name is $name and age is $age")
+}
+
+/*
+Output
+sujit
+19
+Name is Shreyash and age is 17
+ */
